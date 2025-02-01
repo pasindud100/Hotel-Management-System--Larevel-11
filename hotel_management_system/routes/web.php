bd75778ed3995fcb43867795884357c58d593f51
Pasindu Dilshan <dilshanp5400@gmail.com>
2025-02-01 16:24:09 +0530

    Create the project with initial project structure.Inplement hotel model and its routes.

---
+++ hotel_management_system/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\HotelController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('storehotel',[HotelController::class,'store']);
+Route::get('allhotels',[HotelController::class,'index']);
+Route::post('updatehotel/{id}',[HotelController::class,'update']);
+Route::post('deletehotel',[HotelController::class,'destroy']);
